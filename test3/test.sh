79f98e5c195e152adc429126a243e035a6273187
Taylor Monacelli <taylormonacelli@gmail.com>
2023-09-17 07:34:34 -0700

    Add docker test

--- test3/test.sh
+++ test3/test.sh
@@ -10,4 +10,5 @@ if pgrep fluetnd; then
     pkill -f fluentd
 fi
 
-fluentd --verbose --config ./fluentd.yaml
+fluentd --verbose --config ./fluentd.yaml &
+echo '{"project":"p1","script_name":"s1","message":"m1"}' | /opt/fluent/bin/fluent-cat debug.test
